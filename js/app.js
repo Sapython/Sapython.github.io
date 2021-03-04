433372fc5af32b37a9ce0f40698d644b53f3c063
Kumar Saptam <saptampro2003@gmail.com>
2021-03-04 12:47:54 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', '../Assets/particles.json', function() {
+particlesJS.load('particles-js', 'https://raw.githubusercontent.com/Sapython/Sapython.github.io/master/Assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
