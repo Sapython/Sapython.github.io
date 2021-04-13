4b4a646dca72446fa02ba5fe22f90b31557f873d
Kumar Saptam <saptampro2003@gmail.com>
2021-04-13 16:35:18 +0530

    Added some js files into one.

--- js/app.js
+++
@@ -1,3 +0,0 @@
-particlesJS.load('particles-js', 'https://cdn.jsdelivr.net/gh/Sapython/Sapython.github.io@master/assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
